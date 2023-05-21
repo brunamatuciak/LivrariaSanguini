7b72b10c7e9a5fc3d3ff244b9a6b586d0320658c
brunamatuciak <bruna.guedes@sptech.school>
2023-05-20 21:13:51 -0300

    Update dos arquivos

---
+++ main.js
@@ -0,0 +1,179 @@
+var jsonLivros = [{
+    id: 0,
+    titulo: '',
+    urlImg: '',
+    urlImgInfo: '',
+    genero: '',
+    subgenero: '',
+    trechodoLivro: '',
+    comentario1: '',
+    comentario2: '',
+}];
+
+jsonLivros = [{
+    id: 1,
+    titulo: 'O Fim da Eternidade -  por Isaac Asimov',
+    urlImg: 'oFimdoAmanha.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Viagem no tempo',
+    trechodoLivro: 'Acima de tudo, um Técnico deve ser um desapaixonado. A Mudança de Realidade iniciada por ele pode afetar a vida de cinquenta bilhões de pessoas. Cerca de um milhão dessas pessoas talvez sejam tão drasticamente afetadas que poderão ser consideradas novos indivíduos. Sob essas condições, atitudes emocionais constituem uma séria desvantagem.',
+    comentario1: '"Um livro atemporal, feito para emocionar."<br> - THE INDEPENDENT',
+    comentario2: '“Uma obra-prima de um esplendor pungente... desoladora, absoluta e completamente brilhante.”<br> - THE GUARDIAN',
+},
+{
+    id: 2,
+    titulo: 'Eu, Robô -  por Isaac Asimov',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Cyberpunk',
+    trechodoLivro: 'As pessoas dizem: “Está na cara”. Mas que partes da sua cara você consegue ver, a não ser que alguém lhe mostre um espelho? <br> Pode-se provar qualquer coisa que se queira com a razão friamente lógica... se forem escolhidos os postulados apropriados.',
+    comentario1: '"Verdadeiro marco na história da ficção científica."<br> - THE NEW YORK TIMES',
+    comentario2: '“Uma  verdadeira obra-prima da ficção científica.”<br> - THE GUARDIAN',
+},
+{
+    id: 3,
+    titulo: 'A Guerra dos Tronos : As Crônicas de Gelo e Fogo -  por George R. R. Martin',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Fantasia',
+    subgenero: 'Alta fantasia',
+    trechodoLivro: 'Nunca se esqueça do que é, porque é certo que o mundo não se esquecerá. Faça disso sua força. Assim, não poderá ser nunca a sua fraqueza. Arme-se com essa lembrança, e ela nunca poderá ser usada para magoá-lo.',
+    comentario1: '"A guerra dos tronos é a maior obra de fantasia desde que Bilbo encontrou o Anel."<br> - SF Reviews   ',
+    comentario2: '“Uma obra-prima de um esplendor pungente... desoladora, absoluta e completamente brilhante.”<br> - THE GUARDIAN',
+},
+{
+    id: 4,
+    titulo: 'Duna -   por Frank Herbert',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Space Opera',
+    trechodoLivro: 'Não se pode entender um processo interrompendo-o. O entendimento precisa acompanhar o fluxo do processo, tem de se juntar a ele e fluir com ele.',
+    comentario1: '"Testemunhe um clássico absoluto da ficção científica."<br> - THE INDEPENDENT',
+    comentario2: '“Uma obra-prima de um esplendor pungente... desoladora, absoluta e completamente brilhante.”<br> - THE GUARDIAN',
+},
+{
+    id: 5,
+    titulo: 'Neuromancer -  por William Gibson',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Cyberpunk',
+    trechodoLivro: 'Night City era como uma experiência malsucedida de darwinismo social, projetada por um pesquisador entediado que não tirava o dedo do botão de fast-forward.x',
+    comentario1: '"Esta obra distópica, antevê, de modo muito preciso, vários aspectos fundamentais da sociedade atual e de sua relação com a tecnologia."<br> - THE INDEPENDENT',
+    comentario2: '“Uma obra-prima de um esplendor pungente... desoladora, absoluta e completamente brilhante.”<br> - THE GUARDIAN',
+},
+{
+    id: 6,
+    titulo: '1984  - por George Orwell',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Universo Paralelo',
+    trechodoLivro: 'Você não percebe que o verdadeiro objetivo do Novidioma é estreitar o limite do pensamento? No final, vamos tornar o crime de pensamento literalmente impossível, porque não vão existir palavras pelas quais expressá-lo.',
+    comentario1: '"O maior escritor do século XX."<br> - Observer',
+    comentario2: '“Obra-prima terminal de Orwell, 1984 é uma leitura absorvente e indispensável para a compreensão da história moderna.”<br> - New York Review of Books',
+},
+{
+    id: 7,
+    titulo: 'Count Zero  -  por William Gibson',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Cyberpunk',
+    trechodoLivro: 'E, por um instante, ela olhou diretamente para aqueles olhos azuis suaves e soube, com uma certeza instintiva de mamífero, que os excessivamente ricos não eram mais nem remotamente humanos.',
+    comentario1: '"Nesse cenário frenético e superconectado, Gibson conseguiu criar um universo incrivel"<br> - Observer',
+    comentario2: '“Obra-prima terminal de Orwell, 1984 é uma leitura absorvente e indispensável para a compreensão da história moderna.”<br> - New York Review of Books',
+},
+{
+    id: 8,
+    titulo: 'Mona Lisa Overdrive  - por William Gibson',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Cyberpunk',
+    trechodoLivro: '“A matrix tem suas raízes em games de fliperama primitivos”, disse uma voz em off, “nos primeiros programas gráficos e experiências militares com plugues cranianos.”',
+    comentario1: '"O maior escritor do século XX."<br> - Observer',
+    comentario2: '“Obra-prima terminal de Orwell, 1984 é uma leitura absorvente e indispensável para a compreensão da história moderna.”<br> - New York Review of Books',
+},
+{
+    id: 9,
+    titulo: 'Blade Runner  - por Philip K. Dick',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Cyberpunk',
+    trechodoLivro: 'Empatia, evidentemente, existia apenas na comunidade humana, ao passo que inteligência em qualquer grau poderia ser encontrada em todo filo ou ordem biológica, incluindo os aracnídeos.',
+    comentario1: '"Atemporal, esse é o livro que originou um dos maiores filmes do cinema sci-fi."<br> - Observer',
+    comentario2: '“Obra mais popular de Philip K. Dick, Blade Runner moldou parte do universo cyberpunk que conhecemos hoje. ”<br> - New York Review of Books',
+},
+{
+    id: 10,
+    titulo: 'As correntes do espaço  - por Isaac Asimov',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Ficção Científica',
+    subgenero: 'Império Galáctico',
+    trechodoLivro: 'Você não percebe que o verdadeiro objetivo do Novidioma é estreitar o limite do pensamento? No final, vamos tornar o crime de pensamento literalmente impossível, porque não vão existir palavras pelas quais expressá-lo.',
+    comentario1: '"Aventura complexa com temas anti-racistas"<br> - Observer',
+    comentario2: '“Obra-prima terminal de Orwell, 1984 é uma leitura absorvente e indispensável para a compreensão da história moderna.”<br> - New York Review of Books',
+},
+{
+    id: 11,
+    titulo: 'O Senhor dos Anéis: A Sociedade do Anel - por J.R.R. Tolkien',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Fantasia',
+    subgenero: 'Alta fantasia',
+    trechodoLivro: 'Não conheço a metade de vocês a metade do que gostaria; e gosto de menos da metade de vocês a metade do que merecem.',
+    comentario1: '"O maior escritor do século XX."<br> - Observer',
+    comentario2: '“Obra-prima terminal de Orwell, 1984 é uma leitura absorvente e indispensável para a compreensão da história moderna.”<br> - New York Review of Books',
+},
+{
+    id: 12,
+    titulo: 'O Senhor dos Anéis: As duas torres  - por J.R.R. Tolkien',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Fantasia',
+    subgenero: 'Alta fantasia',
+    trechodoLivro: 'O bem e o mal não mudaram desde antanho; nem são uma coisa entre os Elfos e os Anãos e outra entre os Homens. É papel do homem distingui-los, tanto na Floresta Dourada quanto em sua própria casa.',
+    comentario1: '"O maior escritor do século XX."<br> - Observer',
+    comentario2: '“Obra-prima terminal de Orwell, 1984 é uma leitura absorvente e indispensável para a compreensão da história moderna.”<br> - New York Review of Books',
+},
+{
+    id: 13,
+    titulo: 'O Senhor dos Anéis: O retorno do rei - por J.R.R. Tolkien',
+    urlImg: 'euRobo.jpg',
+    urlImgInfo: 'oFimdaEternidade.png',
+    genero: 'Fantasia',
+    subgenero: 'Alta fantasia',
+    trechodoLivro: 'Você não percebe que o verdadeiro objetivo do Novidioma é estreitar o limite do pensamento? No final, vamos tornar o crime de pensamento literalmente impossível, porque não vão existir palavras pelas quais expressá-lo.',
+    comentario1: '"O maior escritor do século XX."<br> - Observer',
+    comentario2: '“Obra-prima terminal de Orwell, 1984 é uma leitura absorvente e indispensável para a compreensão da história moderna.”<br> - New York Review of Books',
+},
+];
+
+
+function mostrar(idLivro) {
+    for (var i = 0; i < jsonLivros.length; i++) {
+        if (jsonLivros[i].id == idLivro) {
+            console.log(jsonLivros[i].titulo);
+
+
+            container.style = "display: none";
+            mostrarLivro.style = "display: block";
+
+
+            titulo.innerHTML = jsonLivros[i].titulo;
+            urlImg.src = "./img/imgCapaLivros/" + jsonLivros[i].urlImg;
+            urlImgInfo.src = "./img/imgInfoLivros/" + jsonLivros[i].urlImgInfo;
+            genero.innerHTML = jsonLivros[i].genero;
+            subgenero.innerHTML = jsonLivros[i].subgenero;
+            trechodoLivro.innerHTML = jsonLivros[i].trechodoLivro;
+            comentario1.innerHTML = jsonLivros[i].comentario1;
+            comentario2.innerHTML = jsonLivros[i].comentario2;
+        }
+    }
+}
